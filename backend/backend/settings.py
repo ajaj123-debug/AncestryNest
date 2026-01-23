0f63f8946de3cbc92d4eaeef2780e031d014ec34
ajaj123-debug <ajaj42699@gmail.com>
2026-01-23 22:00:40 +0530

    p

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -5,8 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-4!mtadwg^6@81^9r+g(0x&c8!9rp%l4md!cez**vj)z5-2qi!x'
